568311d04c032298a71da1c9f88589ee306622d5
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-02-25 16:25:59 +0600

    added chatgpt project

--- portfolio_2.0/src/components/Prjoects.js
+++ portfolio_2.0/src/components/Prjoects.js
@@ -7,57 +7,66 @@ import Food_Store_2 from "./porfolio projects pics/4.png"
 import portfolio from "./porfolio projects pics/5.png"
 import airbnb from "./porfolio projects pics/6.png"
 import netflix from "./porfolio projects pics/7.png"
+import chatgpt from "./porfolio projects pics/8.png"
 
 function Prjoects({ projectName, projectDetails }) {
-    const projects = [{
-        name: "Gadget Store",
-        details: "This is a full stack web app with firebase authentication. Implemented full payment process with stripe payment.",
-        imgSrc: ecomerce,
-        github: "https://github.com/avijitsaha2997/Project-My-E_commerce",
-        liveDemo: "https://e-commerce-2fac1.web.app/"
-    },
-    {
-        name: "Netflix Clone",
-        details: "Netflix clone 2.0. Fully functional netflix clone using with Redux and Stripe payment. Managed all UI information with TMDB api.",
-        imgSrc: netflix,
-        github: "https://github.com/avijitsaha2997/netflix-clone",
-        liveDemo: "https://netflix-clone-eight-weld.vercel.app/"
-    },
-    {
-        name: "Food Store 1",
-        details: "Simply created UI with react js. And manage the full single page application using with react router.",
-        imgSrc: Food_Store_1,
-        github: "https://github.com/avijitsaha2997/Project-Food-Delivery-App-1",
-        liveDemo: "https://food-delivey-app-71f77.web.app/"
-    },
-    {
-        name: "Food Store 2",
-        details: "It's an amazing project where i used context_api for managing state. And this project is connected with firebase hosting.",
-        imgSrc: Food_Store_2,
-        github: "https://github.com/avijitsaha2997/Project-Food-Delivery-App-2",
-        liveDemo: "https://project-food-delivery-app-2.vercel.app/"
-    },
-    {
-        name: "Spotify Clone",
-        details: "I worked on only user interface in this project. Also used original spotify authentication and spotify API.",
-        imgSrc: spotify,
-        github: "https://github.com/avijitsaha2997/Project-Spotify",
-        liveDemo: "https://spotify-clone-cdad8.web.app/"
-    },
-    {
-        name: "Sample Portfolio",
-        details: "This is a sample portfolio using with html, css and javascript only. Worked with form validation at contact section.",
-        imgSrc: portfolio,
-        github: "https://github.com/avijitsaha2997/Portfolio-Core-JS",
-        liveDemo: "https://portfolio-core-js.vercel.app/"
-    },
-    {
-        name: "Airbnb",
-        details: "This is a Next.js project bootstrapped with create-next-app. In this project i used Date-Picker, MapBox Studio, Middleware function.",
-        imgSrc: airbnb,
-        github: "https://github.com/avijitsaha2997/Project-Airbnb",
-        liveDemo: "https://project-airbnb-pi.vercel.app/"
-    },
+    const projects = [
+        {
+            name: "ChatGPT 2.0",
+            details: "ChatGPT clone with REACT! (Next.js 13, Firebase, Tailwind CSS, TypeScript). I used ChatGPT API to create my own AI chat bot. Firebase v9 used to create & store REALTIME chats with chatGPT. Manage to reduce the overall number of Bugs and Errors with TypeScript.",
+            imgSrc: chatgpt,
+            github: "https://github.com/avijitsaha2997/ChatGPT-clone",
+            liveDemo: "https://chat-gpt-clone-chi-seven.vercel.app"
+        },
+        {
+            name: "Gadget Store",
+            details: "This is a full stack web app with firebase authentication. Implemented full payment process with stripe payment.",
+            imgSrc: ecomerce,
+            github: "https://github.com/avijitsaha2997/Project-My-E_commerce",
+            liveDemo: "https://e-commerce-2fac1.web.app/"
+        },
+        {
+            name: "Netflix Clone",
+            details: "Netflix clone 2.0. Fully functional netflix clone using with Redux and Stripe payment. Managed all UI information with TMDB api.",
+            imgSrc: netflix,
+            github: "https://github.com/avijitsaha2997/netflix-clone",
+            liveDemo: "https://netflix-clone-eight-weld.vercel.app/"
+        },
+        {
+            name: "Food Store 1",
+            details: "Simply created UI with react js. And manage the full single page application using with react router.",
+            imgSrc: Food_Store_1,
+            github: "https://github.com/avijitsaha2997/Project-Food-Delivery-App-1",
+            liveDemo: "https://food-delivey-app-71f77.web.app/"
+        },
+        {
+            name: "Food Store 2",
+            details: "It's an amazing project where i used context_api for managing state. And this project is connected with firebase hosting.",
+            imgSrc: Food_Store_2,
+            github: "https://github.com/avijitsaha2997/Project-Food-Delivery-App-2",
+            liveDemo: "https://project-food-delivery-app-2.vercel.app/"
+        },
+        {
+            name: "Spotify Clone",
+            details: "I worked on only user interface in this project. Also used original spotify authentication and spotify API.",
+            imgSrc: spotify,
+            github: "https://github.com/avijitsaha2997/Project-Spotify",
+            liveDemo: "https://spotify-clone-cdad8.web.app/"
+        },
+        {
+            name: "Sample Portfolio",
+            details: "This is a sample portfolio using with html, css and javascript only. Worked with form validation at contact section.",
+            imgSrc: portfolio,
+            github: "https://github.com/avijitsaha2997/Portfolio-Core-JS",
+            liveDemo: "https://portfolio-core-js.vercel.app/"
+        },
+        {
+            name: "Airbnb",
+            details: "This is a Next.js project bootstrapped with create-next-app. In this project i used Date-Picker, MapBox Studio, Middleware function.",
+            imgSrc: airbnb,
+            github: "https://github.com/avijitsaha2997/Project-Airbnb",
+            liveDemo: "https://project-airbnb-pi.vercel.app/"
+        },
 
     ];
     return (
